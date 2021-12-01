c62553ebaec2908614bd65893b1e3e2892b5f1f6
Silvain <kaboresylvain01@gmail.com>
2021-12-01 14:39:06 +0100

    fin

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
+#include <SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-//#include <conio.h>
+#include <conio.h>
 #include <string.h>
+
+
 //definition structure pour le groupe
 typedef struct Cluster {
 	int medoid;
@@ -18,6 +21,8 @@ typedef struct Point {
 	float Malice;
 	P_Cluster cluster;
 }Point;
+typedef Point* P_Point ;
+
 //definition structure couleur
 typedef struct Color{
     int r;
@@ -25,36 +30,41 @@ typedef struct Color{
     int b;
 }Color;
 
-typedef Point* P_Point ;
+
 //prototypes des fonctions
-int minimum(int,int);
-int maximum(int,int);
+int min(int,int);
+int max(int,int);
 float getDistance(Point,Point);
-void afficheCluster(Cluster);
+void afficheCluster(P_Cluster);
 void initialisePoints(P_Point,int);
 void initialiseClusters(P_Point,P_Cluster,int,int);
 void initialiseDistance(float *distance[],P_Point,int);
+void affecterPointsMedoideProche(P_Point,P_Cluster, float *distance[], int, int);
+Cluster selectBestClusterMedoids(P_Point, float *distance[], Cluster , int , SDL_Renderer * ,Color);
+void pam( int, int, SDL_Window*);
+
+
+
+
+
 
 //Definition des fonctions
 //retourne le min de 2 valeur
-int minimum(int i , int j){
+int min(int i , int j){
     int m=(i<j) ? i:j;
 	return m;
 }
-
 //retourne le max de 2 valeur
-int maximum(int i , int j){
+int max(int i , int j){
     int m=(i>j) ? i:j;
 	return m;
 }
-
 //fonction qui retourne distance entre 2points
-//Ici chaque point est representé dans le plan par 4 coordonnées (4D)
 float getDistance(Point a,Point b){
     float s=sqrt(pow(a.Courage-b.Courage,2)+pow(a.Loyaute-b.Loyaute,2)+ pow(a.Sagesse-b.Sagesse,2)+
                  pow(a.Malice-b.Malice,2));
 	return s;
-
+}
 //fonction pour l'intialisation de n points
 void initialisePoints(P_Point points,int n){
 	int i;
@@ -66,22 +76,25 @@ void initialisePoints(P_Point points,int n){
 		printf("Can't open file\n");
 
 	else {
-		//Ici on a pris un tableau de taille 1084
+		// Here we have taken size of
+		// array 1024 you can modify it
 		char buffer[1024];
 
 		int row = 0;
 		int column = 0;
 
-		while (fgets(buffer,1024, fp)) {
+		while (fgets(buffer,
+					1024, fp)) {
 			column = 0;
 			row++;
 
-			// Pour éviter l'affichage de la colonne
-			// les noms dans le fichier peuvent être modifiés si besoin
+			// To avoid printing of column
+			// names in file can be changed
+			// according to need
 			if (row == 1)
 				continue;
 
-			// Division des données
+			// Splitting the data
 			char* value = strtok(buffer, ";");
 
 			while (value) {
@@ -121,7 +134,7 @@ void initialisePoints(P_Point points,int n){
 
 				// Column 6
 				if (column == 5) {
-					printf("\tClan :");
+					printf("\tVille :");
 				}
 
 				printf("%s", value);
@@ -136,8 +149,14 @@ void initialisePoints(P_Point points,int n){
 		fclose(fp);
 	}
 
-}
+	/*for(i=0;i<n;i++){
+        points[i].Courage=rand()%(40 + 1) + 1;
+        points[i].Loyaute=rand()%(30 + 1) + 1;
+        points[i].Sagesse=rand()%(30 + 1) + 1;
+        points[i].Malice =rand()%(30 + 1) + 1;
 
+    } */
+}
 // fonction pour afficher un cluster
 void afficheCluster(P_Cluster cluster)
 {
@@ -163,20 +182,146 @@ void initialiseDistance(float *distance[],P_Point points,int n){
 	}
 }
 
+//fonction affectation des points au medoid le plus proche
+void affecterPointsMedoideProche(P_Point points, P_Cluster clusters, float *distance[], int n, int k){
+	int i,j;
+	for(i=0;i<k;i++)
+		clusters[i].taille=0;
+	for(i=0;i<n;i++){
+		Cluster *cluster=&clusters[0];
+		int mid = cluster->medoid;
+		for(j=1;j<k;j++){
+			Cluster *tmpCluster=&clusters[j];
+			int tmpmid=tmpCluster->medoid;
+			if(distance[max(i,tmpmid)][min(i,tmpmid)]<distance[max(i,mid)][min(i,mid)]){
+				cluster=tmpCluster;
+				mid=cluster->medoid;
+			}
+		}
+		points[i].cluster=cluster;
+		cluster->taille++;
+	}
+}
+//fonction retournant le meilleur group
+Cluster selectBestClusterMedoids(P_Point points, float *distance[], Cluster cluster , int n, SDL_Renderer * renderer,Color color){
+	int i,j=0;
+	float totalDist=0;
+	float tmpTotalDist=0;
+	int* medoidPoints=(int*)malloc(sizeof(int)*cluster.taille);
+	int medoid=cluster.medoid;
+	for(i=0;i<n;i++){
+        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b,255);
+		if(points[i].cluster->medoid==cluster.medoid){
+            SDL_RenderDrawLine(renderer,points[medoid].Courage*15,points[medoid].Loyaute*15,points[i].Courage*15,points[i].Loyaute*15);
+            SDL_RenderPresent(renderer);
+			medoidPoints[j]=i;
+			j++;
+		}
+	}
+	for(i=0;i<n;i++)
+		totalDist+=distance[max(i,medoid)][min(i,medoid)];
 
-	int main(int argc, char** argv)
+	for(i=0;i<cluster.taille;i++){
+		tmpTotalDist=0;
+		int tmpMedoid=medoidPoints[i];
+
+		for(j=0;j<n;j++)
+			tmpTotalDist+=distance[max(j,tmpMedoid)][min(j,tmpMedoid)];
+
+		if(tmpTotalDist<totalDist){
+			cluster.medoid=tmpMedoid;
+			totalDist=tmpTotalDist;
+		}
+	}
+	free(medoidPoints);
+	return cluster;
+}
+//fonction qui applique l'algorithme KMedoids (PAM)
+void pam( int n, int k, SDL_Window * window){
+    //Code--
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+    P_Point points=(P_Point)malloc(n*sizeof(Point));
+	P_Cluster clusters=(P_Cluster)malloc(k*sizeof(Cluster));
+    int i,j,changeTest;
+    float *distance[n];
+	for(i=0;i<n;i++){
+			distance[i]=(float*)malloc((i+1)*sizeof(float));
+	}
+    Color color[k];
+	for(i=0,j=k;i<k;i++,j--)
+        {
+            color[i].r=j*200;
+            color[i].g=i*80;
+            color[i].b=j*130;
+        }
+	initialisePoints(points,n);
+	initialiseClusters(points, clusters , n, k);
+	initialiseDistance(distance,points,n);
+	do{
+		changeTest=0;
+		P_Cluster tmpClusters=(P_Cluster)malloc(sizeof(Cluster)*k);
+		affecterPointsMedoideProche(points,clusters,distance,n,k);
+		for(i=0;i<k;i++){
+		tmpClusters[i]=selectBestClusterMedoids(points,distance,clusters[i],n,renderer,color[i]);
+		}
+		for(i=0;i<k;i++)
+			if(tmpClusters[i].medoid != clusters[i].medoid){
+				changeTest=1;
+				afficheCluster(tmpClusters);
+				break;
+			}
+		if(changeTest){
+            clusters=tmpClusters;
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0,255);
+            SDL_RenderClear(renderer);
+		}
+
+	}while(changeTest==1);
+
+    for(i=0;i<k;i++){
+            int medoid=clusters[i].medoid;
+            SDL_SetRenderDrawColor(renderer, color[i].r, color[i].g, color[i].b,255);
+            for(j=0;j<n;j++){
+                if(points[j].cluster->medoid==medoid){
+                    SDL_Rect rectangle;
+                    rectangle.x = points[j].Courage*15;
+                    rectangle.y = points[j].Loyaute*15;
+                    rectangle.w = 4;
+                    rectangle.h = 4;
+                    SDL_RenderFillRect(renderer, &rectangle);
+                }
+            }
+        }
+    SDL_RenderPresent(renderer);
+}
+int main(int argc, char** argv)
 {
+    int quit = 0;
+    SDL_Event event;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("PAM Algorithm Implementing",100, 100, 400, 400, 0);
+
+
 	int n=50;//points
-	int k=6;//clusters
-	if(k>n)
-			exit(0);
+	int k=6;//groupes
+	if(k>n)exit(0);
 
-	afficheCluster(....);
-    
+    pam(n,k,window);
 
-   
+    while (!quit)
+    {
+        SDL_WaitEvent(&event);
 
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = 1;
+            break;
+        }
+    }
 
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
